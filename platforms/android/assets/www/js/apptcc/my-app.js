6b0036a33588bb3b8115b9e2122777cde51fc003
Francisco Schwertner <rockchico@gmail.com>
2017-07-27 20:28:03 -0300

    correção espaçamento após navbar

--- platforms/android/assets/www/js/apptcc/my-app.js
+++ platforms/android/assets/www/js/apptcc/my-app.js
@@ -19,8 +19,8 @@
  */
 
 //var appTccServer = "192.168.0.114";
-//var appTccServer = "10.1.0.3";
-var appTccServer = "ec2-13-58-57-157.us-east-2.compute.amazonaws.com";
+var appTccServer = "10.1.0.3";
+//var appTccServer = "ec2-13-58-57-157.us-east-2.compute.amazonaws.com";
 
 
 
